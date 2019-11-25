a8fb6c97e45df34650744f92033e41adc4461966
Martín Mantovani <martin.mantovani@gmail.com>
2019-11-25 13:33:53 -0300

    init

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,29 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+
+  @ViewChild(IonInfiniteScroll, {static : false}) infiniteScroll: IonInfiniteScroll;
+
+  constructor() {
+
+
+  }
+
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      event.target.complete();
+
+      // App logic to determine if all data is loaded
+      // and disable the infinite scroll
+     /* if (data.length == 1000) {
+        event.target.disabled = true;
+      }*/
+    }, 500);
+  }
+
+  toggleInfiniteScroll() {
+    this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
+  }
 
 }
